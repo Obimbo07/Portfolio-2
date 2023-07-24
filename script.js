be019e1cbadb309271b3a82c0355a3037c302420
Austin Obimbo <118368849+Obimbo07@users.noreply.github.com>
2023-07-25 00:14:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const popup = document.getElementById('mobile-menu');
 function openMenu() {
   popup.classList.add('dropdown_show');
 }
+openMenu();
 
 function closeMenu(){
   popup.classList.remove('dropdown_show');
 }
+closeMenu();
