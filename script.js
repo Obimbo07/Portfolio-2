e869ce7d10e971da64121511f4852e9306e4f837
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-07-24 21:21:25 +0300

    Add funcionality to humburger menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const popup = document.getElementById('mobile-menu');
+const menuDisappear = document.querySelectorAll('mobile-links')
+
+function openMenu() {
+  popup.classList.add('dropdown_show');
+}
